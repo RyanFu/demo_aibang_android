bc79a6aaf26adf3f585fc15328ad86769ed8dcbd
fydx <lbgg918@gmail.com>
2013-05-02 23:11:26 +0800

    Client更改

--- src/com/sssta/demo_aibang_android/Client.java
+++ src/com/sssta/demo_aibang_android/Client.java
@@ -3,6 +3,7 @@ package com.sssta.demo_aibang_android;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -12,15 +13,18 @@ import android.util.Log;
 
 public class Client {
 	private String result = null;
-
+	private OutputStream outputStream = null;
 	public String ReceiveFromServer() throws IOException
 	{
+		
 		BufferedReader is = null;
 		try {
 			Socket socket=new Socket("192.168.1.135",4700);
 			//向本机的4700端口发出客户请求
 			BufferedReader sin=new BufferedReader(new InputStreamReader(System.in));
 			//由系统标准输入设备构造BufferedReader对象
+			outputStream = socket.getOutputStream();
+			
 			PrintWriter os=new PrintWriter(socket.getOutputStream());
 			//由Socket对象得到输出流，并构造PrintWriter对象
 			is=new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -34,15 +38,15 @@ public class Client {
 			//刷新输出流，使Server马上收到该字符串
 			//		System.out.println("Client:"+readline);
 			//在系统标准输出上打印读入的字符串
-			System.out.println("Server:"+is.readLine());
+			//System.out.println("Server:"+is.readLine());
 			Log.e("normal1", "success");
 			result = is.readLine();
+			Log.i("info", result);
 			Log.e("normal2", "success");
 				//从Server读入一字符串，并打印到标准输出上
 			//readline=sin.readLine(); //从系统标准输入读入一字符串
 			os.close(); //关闭Socket输出流
 			is.close(); //关闭Socket输入流
-		
 			socket.close(); //关闭Socket
 		
 		} catch (Exception e) {
