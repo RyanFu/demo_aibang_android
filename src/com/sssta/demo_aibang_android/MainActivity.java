90f928e250c98e5c9f0662533fd5e40e3e8bda24
fydx <lbgg918@gmail.com>
2013-05-03 23:54:54 +0800

    使用asynctask的方式进行线程处理

--- src/com/sssta/demo_aibang_android/MainActivity.java
+++ src/com/sssta/demo_aibang_android/MainActivity.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import com.sssta.demo_aibang_android.R.string;
 
+import android.R.integer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 public class MainActivity extends FragmentActivity {
 	private String resultString = null;
 	private TextView testTextView;
+	public static final String ipAddress = "192.168.1.135"; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,8 +23,10 @@ public class MainActivity extends FragmentActivity {
 	//	SimpleListFragment simpleListFragment = new SimpleListFragment();
 	//	getSupportFragmentManager().beginTransaction().replace(R.id.container, simpleListFragment).commit();
 		testTextView =(TextView) findViewById(R.id.test_testview);
-		
-		 new Thread(new Runnable() {
+		/*
+		 *使用post的方式把对ui tookit的控制加入到主线程
+		 */
+		 /*new Thread(new Runnable() {
 		        public void run() {
 		        try {
 					Client client = new Client();
@@ -44,9 +49,11 @@ public class MainActivity extends FragmentActivity {
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}
+			  
 		        }
-		    }).start();
+		    }).start();*/
+		new DownloadTask().execute();
+		
 		
 		
 	}
@@ -57,5 +64,31 @@ public class MainActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	/*
+	 * 使用asnyctask方式进行线程处理
+	 */
+	private class DownloadTask extends AsyncTask<Void, integer, String>
+	{
+		
+		
+		@Override
+		protected String doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			Client client = new Client();
+        	try {
+				resultString = client.ReceiveFromServer();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return resultString;
+			
+		}
+		 protected void onPostExecute(String result) {
+	        testTextView.setText(result);
+	     }
+			
+		}
+		
+	}
 
-}
